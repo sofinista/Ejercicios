d5327e42a8799ed42f1f5692a5b874a3e59224bd
sofinista <asofiabc88@gmail.com>
2026-02-07 16:00:57 -0500

    Ejercicio 2: edad en 10 años

---
+++ edad.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class edad {
+    public static void main(String[] args) {
+        String nombre = JOptionPane.showInputDialog("¿Cómo te llamas?");
+        String edad = JOptionPane.showInputDialog("¿Cúal es tu edad?");
+        JOptionPane.showMessageDialog(null, "Hola  " + nombre + " tu edad en 10 años será: " + ( Integer.parseInt(edad) + 10) + "años");
+
+    }
+}
