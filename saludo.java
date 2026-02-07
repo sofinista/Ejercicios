55b7f040a8c69c30bc0b9434a849df8f2cae2510
sofinista <asofiabc88@gmail.com>
2026-02-07 15:43:01 -0500

    Ejercicio saludo y nombre

---
+++ saludo.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class saludo {
+    public static void main(String[] args) {
+            String nombre = JOptionPane.showInputDialog("Como te llamas?");
+        System.out.println("Hola " + nombre);
+    } 
+}
